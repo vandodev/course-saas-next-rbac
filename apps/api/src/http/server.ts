38788337ff1db250ebcb7d32fbe02b52e74862f1
vandodev <vandoaparecido@hotmail.com>
2024-04-21 07:50:58 -0300

    :construction_worker: :cop: :money_with_wings: Back-end - Criando servido com fastify

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,19 @@
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  // jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
