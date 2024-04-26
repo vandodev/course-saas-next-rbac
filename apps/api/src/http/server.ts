53b4bb5e699525fce74718ac64e4d577c4f835ac
vandodev <vandoaparecido@hotmail.com>
2024-04-26 08:06:00 -0300

    :construction_worker: :cop: :money_with_wings: Back-end - Buscando todas as organizações de usuário logado

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,7 @@ import { resetPassword } from '@/http/routes/auth/reset-password'
 import { createOrganization } from '@/http/routes/orgs/create-organization'
 import { getMembership } from '@/http/routes/orgs/get-membership'
 import { getOrganization } from '@/http/routes/orgs/get-organization'
+import { getOrganizations } from '@/http/routes/orgs/get-organizations'
 
 import { createAccount } from './routes/auth/create-account'
 
@@ -69,6 +70,7 @@ app.register(resetPassword)
 app.register(createOrganization)
 app.register(getMembership)
 app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
