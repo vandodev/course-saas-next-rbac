52696369571eacbf3f81abc75236b62fb87b43a0
vandodev <vandoaparecido@hotmail.com>
2024-06-08 07:43:06 -0300

    :construction_worker: :cop: :money_with_wings: Web - Requisições http com ky

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,5 +1,22 @@
 'use server'
 
+import ky from 'ky'
+
+const api = ky.create({
+  prefixUrl: 'http://localhost:3333',
+})
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await api
+    .post('sessions/password', {
+      json: {
+        email,
+        password,
+      },
+    })
+    .json()
+
+  console.log(result)
 }
