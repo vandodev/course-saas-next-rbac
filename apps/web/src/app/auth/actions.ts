73121ecfb959cf4e307e25e0c3b4cfbbc306d8ac
vandodev <vandoaparecido@hotmail.com>
2024-06-10 10:01:22 -0300

    :construction_worker: :cop: :money_with_wings: Web - Obtendo usuário autenticado

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation'
 export async function signInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignInURL.searchParams.set('client_id', '00000')
+  githubSignInURL.searchParams.set('client_id', 'Ov23ctamwCPV4iCHsSIR')
   githubSignInURL.searchParams.set(
     'redirect_uri',
     'http://localhost:3000/api/auth/callback',
