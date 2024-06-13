fd28563db313809458a1a393bf9960cdaa7ce85f
vandodev <vandoaparecido@hotmail.com>
2024-06-13 07:45:57 -0300

    :construction_worker: :cop: :money_with_wings: Web - Resolvendo duplicação de header

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import { Header } from '@/components/header'
 
 export default async function Projects() {
-  return <div className="py-4"></div>
+  return (
+    <div className="space-y-4 py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px]">Projects</main>
+    </div>
+  )
 }
