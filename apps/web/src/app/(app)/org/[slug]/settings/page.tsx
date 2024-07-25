525faec9fcc7ec5fb7db27a1763a3a8c747aef98
vandodev <vandoaparecido@hotmail.com>
2024-07-25 08:48:20 -0300

    :construction_worker: :cop: :money_with_wings: Web - Edição da organização

--- apps/web/src/app/(app)/org/[slug]/settings/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/settings/page.tsx
@@ -1,4 +1,4 @@
-import { ability } from '@/auth/auth'
+import { ability, getCurrentOrg } from '@/auth/auth'
 import {
   Card,
   CardContent,
@@ -9,13 +9,17 @@ import {
 
 import { OrganizationForm } from '../../organization-form'
 import { ShutdownOrganizationButton } from './shutdown-organization-button'
+import { getOrganization } from '@/http/get-organization'
 
 export default async function Settings() {
+  const currentOrg = getCurrentOrg()
   const permissions = await ability()
 
   const canUpdateOrganization = permissions?.can('update', 'Organization')
   const canGetBilling = permissions?.can('get', 'Billing')
   const canShutdownOrganization = permissions?.can('delete', 'Organization')
+
+  const { organization } = await getOrganization(currentOrg!)
 
   return (
     <div className="space-y-4">
@@ -31,14 +35,22 @@ export default async function Settings() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <OrganizationForm />
+              <OrganizationForm
+                isUpdating
+                initialData={{
+                  name: organization.name,
+                  domain: organization.domain,
+                  shouldAttachUsersByDomain:
+                    organization.shouldAttachUsersByDomain,
+                }}
+              />
             </CardContent>
           </Card>
         )}
 
         {canGetBilling && <div>billing</div>}
 
-        {canUpdateOrganization && (
+        {canShutdownOrganization && (
           <Card>
             <CardHeader>
               <CardTitle>Shutdown organization</CardTitle>
