f4cddf0c4bc9500f4bababbbb6db3ddcb29e4f31
vandodev <vandoaparecido@hotmail.com>
2024-07-25 08:15:58 -0300

    :construction_worker: :cop: :money_with_wings: Web - Deletando organização

--- apps/web/src/app/(app)/org/[slug]/settings/shutdown-organization-button.tsx
+++ apps/web/src/app/(app)/org/[slug]/settings/shutdown-organization-button.tsx
@@ -1,9 +1,23 @@
 import { XCircle } from 'lucide-react'
+import { redirect } from 'next/navigation'
+
+import { getCurrentOrg } from '@/auth/auth'
 import { Button } from '@/components/ui/button'
+import { shutdownOrganization } from '@/http/shutdown-organization'
 
 export function ShutdownOrganizationButton() {
+  async function shutdownOrganizationAction() {
+    'use server'
+
+    const currentOrg = getCurrentOrg()
+
+    await shutdownOrganization({ org: currentOrg! })
+
+    redirect('/')
+  }
+
   return (
-    <form>
+    <form action={shutdownOrganizationAction}>
       <Button type="submit" variant="destructive" className="w-56">
         <XCircle className="mr-2 size-4" />
         Shutdown organization
