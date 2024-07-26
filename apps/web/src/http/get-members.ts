3149c775189c4e978bafaf6247d1c89ff44ed4c9
vandodev <vandoaparecido@hotmail.com>
2024-07-26 08:56:57 -0300

    :construction_worker: :cop: :money_with_wings: Web - Removendo mebro

--- apps/web/src/http/get-members.ts
+++ apps/web/src/http/get-members.ts
@@ -15,7 +15,12 @@ interface GetMembersResponse {
 
 export async function getMembers(org: string) {
   const result = await api
-    .get(`organizations/${org}/members`)
+  //para atualizar
+  .get(`organizations/${org}/members`, {
+    next: {
+      tags: [`${org}/members`],
+    },
+  })
     .json<GetMembersResponse>()
 
   return result
