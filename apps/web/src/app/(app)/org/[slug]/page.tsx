fdc973d330b9d0eb46b7ef251a0b15e5f16f72df
vandodev <vandoaparecido@hotmail.com>
2024-07-27 10:07:30 -0300

    :construction_worker: :cop: :money_with_wings: Web - Estrutura dos projetos

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from '@/components/header'
-
-export default async function Projects() {
-  return (
-    <div className="space-y-4">
-      <h1 className="text-2xl font-bold">Project</h1>
-    </div>
-  )
-}
