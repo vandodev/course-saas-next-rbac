9527b51f9e9ca12f52a39b27592182dddefc80ed
vandodev <vandoaparecido@hotmail.com>
2024-07-28 10:16:42 -0300

    :construction_worker: :cop: :money_with_wings: Web - Aceitando comvite sem estar logado

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 import { z } from 'zod'
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
+import { acceptInvite } from '@/http/accept-invite'
 
 const signInSchema = z.object({
   email: z
@@ -33,6 +34,18 @@ export async function signInWithEmailAndPassword(data: FormData) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     })
+
+    const inviteId = cookies().get('inviteId')?.value
+
+    if (inviteId) {
+      try {
+        await acceptInvite(inviteId)
+        cookies().delete('inviteId')
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
